c0dabedb96003d59eff931b891d653196a7f0bf2
Jay Pancholi <jaypancholi94@gmail.com>
2024-10-28 00:47:55 +1100

    fix: removed context

--- src/context/clue-sheet-context.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-type ClueSheetContextType = { something: string };
-export const ClueSheetContext = createContext<ClueSheetContextType | null>(
-  null,
-);
-export const ClueSheetProvider = ({ children }: { children: ReactNode }) => {
-  return <></>;
-};
